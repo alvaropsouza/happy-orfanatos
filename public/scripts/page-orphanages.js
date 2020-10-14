d5ef7f03a89e5e71698d950fb035e46fd2755d08
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-10-13 23:10:08 -0300

    Criado mapa e adicionada localidade de fortaleza, CE

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,8 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-3.7899266,-38.5889876], 14);
+
+// Create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Create icon
+
+L.marker([-3.7899266,-38.5889876]).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
