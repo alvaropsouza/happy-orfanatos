c1a83f6dcc1d28eee1a84bafe720b933163f5417
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-10-18 16:49:03 -0300

    Adicionada template engine hbs

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 // Essa parte é o roteamento para definir como os terminais irão responder as solicitações
 // do cliente mais infos https://expressjs.com/pt-br/guide/routing.html
 
-// Importando o módulo express
+// Importando a dependencia express
 const express = require('express')
+// Dependencia do proprio node js que tira bugs de uso de contrabarra
+const path = require('path')
+
+
+// Iniciando express
 const server = express()
+server
+// Utilizando os arquivos estáticos (Css, images etc...)
+.use(express.static('public'))
+
+//configurar template engine
+.set('views', path.join(__dirname, "view"))
+.set('view engine', 'hbs')
 
 // Criando rota
-server.get('/', () => {
-    console.log('oi');
+.get('/', (request, response) => {
+    return response.render('index')
 })
 
 // Ligar servidor
