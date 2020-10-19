551769e71b3e1985ebe6ff758aa921dc7f896780
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-10-19 17:30:00 -0300

    1:35 aula

---
+++ src/database/fakedata.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-3.7899266",
+        lng: "-38.5889876",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1527490087278-9c75be0b8052?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1047&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência pra dar.",
+        opening_hours: "Horário de visitas das 8 as 18h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-3.7899266",
+        lng: "-38.5879876",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1527490087278-9c75be0b8052?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1047&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência pra dar.",
+        opening_hours: "Horário de visitas das 8 as 18h",
+        open_on_weekends: "0"
+    }  
+]
