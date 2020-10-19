1dd432f97ce4cc0163460500576635197fb0153d
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-10-19 14:14:09 -0300

    Todas as páginas no ar

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,7 +5,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // Create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29,68],
     popupAnchor: [170, 2]
@@ -17,7 +17,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
 
 // Create and add marker
 L.marker([-3.7899266,-38.5889876], {icon}).addTo(map).bindPopup(popup).openPopup();
--- src/pages.js
+++ src/pages.js
@@ -1,8 +1,7 @@
 module.exports = {
     // index: function () {}
     index(req, res) {
-        const city = req.query.city
-        return res.render('index', { city })
+        return res.render('index')
     },
 
     orphanage(req, res) {
